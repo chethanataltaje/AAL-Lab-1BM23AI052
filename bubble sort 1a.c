bd9e0c5028ea0b407387c9d6d9a0ef1f052070ef
chethanataltaje <chethanataltaje@gmail.com>
2025-05-14 23:22:55 +0530

    Add files via upload

---
+++ bubble sort 1a.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++)
+        for (int j = 0; j < n-i-1; j++)
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+}
+int main() {
+    int arr[] = {22,36,17,21,46,34,78};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, n);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    return 0;
+}
